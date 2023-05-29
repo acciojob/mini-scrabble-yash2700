dea5a64de20af90dfb18a21fce484b7c4d50338d
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 14:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+ let evaluatedText=document.getElementById("evaluatedText")
+        let letterCount=document.getElementById("letterCount")
+
+        evaluatedText.addEventListener("input" ,calculatelength)
+        function calculatelength(){
+           letterCount.innerHTML= evaluatedText.value.length
+            document.body.append(letterCount)
+        }
